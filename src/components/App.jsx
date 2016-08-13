1d175edd62023262c007ea7728906bcb713b6c85
omarmung <dkimmel@gmail.com>
2016-08-12 19:57:38 -0700

    Find crazy bug on nextVideos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,51 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideo: exampleVideoData[0],
+    };
+    this.nextVideo = nextVideo.bind(this);
+   
+  }
+
+  nextVideo(index) {
+    console.log('nextVideo THIS', this, 'nextVideo INDEX', index);
+    this.setState({
+      currentVideo: exampleVideoData[index],
+    });
+   //  this.render();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList context={this} videos={exampleVideoData}/>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={exampleVideoData[0]}/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={exampleVideoData}/>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,15 @@
+var nextVideo = (index, props) => {
+  console.log('nextVideo INDEX', index);
+  console.log('nextVideo PROPS', props);
+  console.log('nextVideo THIS', this);
+  debugger;
+  props.context.nextVideo(index);
+};
+
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(function(item) {
-      return <VideoListEntry video={item} key={item.id.videoId}/>;
+    {props.videos.map(function(item, index) {
+      return <VideoListEntry video={item} key={item.id.videoId} dothis={nextVideo.bind(null, index, props)}/>;
     })}
   </div>
 );
@@ -15,4 +23,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
-
+// window.nextVideo = nextVideo;
